155375865619ad8f02273a46a8862ab7e97482f4
jbuick28 <jamie.buick@hiab.com>
2024-05-17 12:41:43 +0100

    test

--- 283-jsx-attributes-and-styling/src/index.jsx
+++ 283-jsx-attributes-and-styling/src/index.jsx
@@ -27,6 +27,8 @@ root.render(
           alt="noodles"
           src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg"/>
       </div>
+
+      
   </div>
 );
 
