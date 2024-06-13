a4681fe2fc5ca07f3e6f3d5635ca26d702401b86
Jamie Buick <jamiebuick28@gmail.com>
2024-06-13 20:31:28 +0100

    More modules completed regarding components

---
+++ 287-react-components-practice/src/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <App />
+);
+
+
+
+
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
