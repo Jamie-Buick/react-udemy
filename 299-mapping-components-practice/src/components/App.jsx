384bd546ece6428b6daed68b3fceda461c068f58
Jamie Buick <jamiebuick28@gmail.com>
2024-06-24 22:30:25 +0100

    Completed modules on creating components, mapping and reactdev tools

---
+++ 299-mapping-components-practice/src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createCard(emojis){
+  return ( 
+    <Entry 
+      id={emojis.id}
+      key={emojis.id}
+      emoji={emojis.emoji}
+      name ={emojis.name}
+      meaning={emojis.meaning}
+  
+    />
+  );
+}
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+        {emojipedia.map(createCard)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
